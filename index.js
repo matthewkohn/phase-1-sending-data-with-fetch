5cf12ddc6c89f55d31216ba87b7d7b0365f96916
Matthew <kohney319m@gmail.com>
2021-10-01 16:20:56 -0600

    commit tree 7745aff3625e6d27be537df25480f662d1a1dea0

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(userName, userEmail) {
+  const destinationURL = 'http://localhost:3000/users';
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    }),
+  };
+
+  return fetch(destinationURL, configurationObject)
+    .then(response => response.json())
+    .then(object => {
+      const p = document.createElement('p');
+      const body = document.querySelector('body');
+
+      p.textContent = object.id;
+      body.append(p);
+      console.log(object);
+    })
+    .catch(error => {
+      const p = document.createElement('p');
+      const body = document.querySelector('body');
+      p.textContent = error.message;
+      body.append(p);
+    });
+}
+
